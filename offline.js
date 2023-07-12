39872155140b43d933c7e8e310918f124423271b
Placerr <137558197+Placerr@users.noreply.github.com>
2023-07-12 15:17:41 +0330

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1689162254,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"giphy-sheet0.png",
+		"giphy-sheet1.png",
+		"giphy-sheet2.png",
+		"untitledsheet-sheet0.png",
+		"9patch.png",
+		"sprite-sheet0.png",
+		"sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"firebase.js"
+	]
+}
